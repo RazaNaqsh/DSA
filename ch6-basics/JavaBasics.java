7b39ab7d119fdc133e2812e62eff96b7c1966c9d
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-03 12:43:20 +0530

    Boilerplate for java

---
+++ ch6-basics/JavaBasics.java
@@ -0,0 +1,8 @@
+public class JavaBasics {
+    public static void name(String args[]) {
+        
+    }
+}
+
+// This is boilerplate code for java, 
+// for any code we've to write this much to run any java file
