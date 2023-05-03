92253d72ece35bf46a339631da48466084f9abdf
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-03 15:26:27 +0530

    Variables

--- ch6-basics/JavaBasics.java
+++ ch6-basics/JavaBasics.java
@@ -1,10 +1,34 @@
-public class JavaBasics {
-    public static void main(String args[]) {
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!");
-    }
-}
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         System.out.println("Hello World!!");
+//         System.out.println("Hello World!!");
+//         System.out.println("Hello World!!");
+//     }
+// }
 
 // This is boilerplate code for java, 
 // for any code we've to write this much to run any java file
+
+// public class JavaBasics {
+//     public static void main(String args[]) {
+//         System.out.println("****");
+//         System.out.println("***");
+//         System.out.println("**");
+//         System.out.println("*");
+//     }
+// }
+
+//? Variables in Java
+
+public class JavaBasics {
+    public static void main(String args[]) {
+        int a = 5;
+        System.out.println(a);
+        int b = 10;
+        System.out.println(b);
+        String name = "Raza";
+        System.out.println(name);
+        a = b;
+        System.out.println(a);
+    }
+}
