73ac6cd7f14c34914d3efbc1e54afbd77131f031
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-03 16:04:49 +0530

    Type conversion

---
+++ ch6-basics/JavaTypeConversion.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class JavaTypeConversion {
+    public static void main(String args[]) {
+        // This is possible,
+        int a = 5;
+        long b = a;
+        System.out.println(b);
+        // But the reverse isnt possible
+        // we cant convert long into int
+    }
+}
+
+//? Conversion can only happen when :
+// * Type compatible
+// * Destination type > Source type ( meaning in size )
+
+// ! byte -> short -> int -> float -> long -> double
+// left ones can be converted to the right ones, but not vice versa
+
+//? This is also called implicit or widening or type conversion in Java
