55d20feaf6df75ec9deeeebbb190fa0861cd0c48
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-05 14:28:53 +0530

    Print till n program

--- Javabasics/JavaCalculator.java
+++ Javabasics/JavaCalculator.java
@@ -4,9 +4,12 @@ public class JavaCalculator {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("Enter first number");
         int a = sc.nextInt();
+        System.out.println("Enter second number");
         int b = sc.nextInt();
 
+        System.out.println("Enter Operator");
         char operator = sc.next().charAt(0);
 
         switch(operator){
---
+++ Javabasics/PrintTillN.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class PrintTillN {
+   public static void main(String args[]) {
+       Scanner sc = new Scanner(System.in);
+
+       System.out.print("Enter N: ");
+       int n = sc.nextInt();
+
+       while (n >= 1) {
+           System.out.println(n);
+           n--;
+       }
+   } 
+}
