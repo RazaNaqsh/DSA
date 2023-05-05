33dea55a925ce04003dd685fa384117d36631238
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-05 15:17:28 +0530

    Star pattern

---
+++ JavaPatterns/StarPattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class StarPattern {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter n: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n;i++){
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
