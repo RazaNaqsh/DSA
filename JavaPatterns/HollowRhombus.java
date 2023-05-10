868f04417defbdf941e1d35f9bba3e4c9ea53f6d
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-10 19:30:21 +0530

    Hollow rhombus

--- JavaPatterns/HollowRhombus.java
+++ JavaPatterns/HollowRhombus.java
@@ -1,16 +1,20 @@
-public class Rhombus {
-    public static void rhombus(int n) {
+public class HollowRhombus {
+    public static void hollowRhombus(int n) {
         for (int row = 1; row <= n; row++) {
             for (int space = 1; space <= n - row; space++) {
                 System.out.print(" ");
             }
             for (int star = 1; star <= n; star++) {
-                System.out.print("*");
+                if(row==1 || row==n|| star ==1 ||star==n){
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
     }
     public static void main(String args[]) {
-        rhombus(8);
+        hollowRhombus(5);
     }
 }
