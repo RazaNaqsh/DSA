6f10cee3a93af8a84889382c87febc149873488e
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-15 19:59:02 +0530

    Bubble sorting

---
+++ BasicSortingAlgorithms/BubbleSort.java
@@ -0,0 +1,30 @@
+public class BubbleSort {
+    public static void bubbleSort(int arr[]) {
+        int swap = 0;
+        for (int i = 0; i <= arr.length - 2; i++) {
+            
+            for (int j = 0; j <= arr.length - 2 - i ; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swap += 1;
+                }
+            }
+        }
+        System.out.println("The sorted arr is:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+        if (swap > 0) {
+            System.out.println("Total no of swaps: "+swap);
+        } else {
+            System.out.println("This array is already Sorted!");
+        }
+    }
+    public static void main(String[] args) {
+        int numbers[] = { 1, 2, 3, 4, 5 };
+        bubbleSort(numbers);
+    }
+}
