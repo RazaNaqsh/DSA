22d5a2fff34ec811bdd03c650da611436555597f
RazaNaqsh <owaisraza1704@gmail.com>
2023-05-22 22:07:17 +0530

    Linear complexity diagonal sum

--- 2DArrays/DiagonalSum.java
+++ 2DArrays/DiagonalSum.java
@@ -2,13 +2,25 @@ public class DiagonalSum {
     public static int diagonalSum(int matrix[][]) {
         int sum = 0;
 
+        // * Brute force method of big O n^2
+        // for (int i = 0; i < matrix.length; i++) {
+        //     for (int j = 0; j < matrix[0].length; j++) {
+        //         if (i == j) {
+        //             sum += matrix[i][j];
+        //         } else if (i + j == matrix.length - 1) {
+        //             sum += matrix[i][j];
+        //         }
+        //     }
+        // }
+
+        // * Linear time complexity
+
         for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if (i == j) {
-                    sum += matrix[i][j];
-                } else if (i + j == matrix.length - 1) {
-                    sum += matrix[i][j];
-                }
+            // pd
+            sum += matrix[i][i];
+            // sd
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - i - 1];
             }
         }
         return sum;
