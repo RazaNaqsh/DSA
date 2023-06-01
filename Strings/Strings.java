ae318d3eef56541bf008611edd630b5751a9bf34
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-01 22:13:03 +0530

    Length function of String

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -4,17 +4,20 @@ public class Strings {
     
     public static void main(String[] args) {
 
-        /* A CHAR ARRAY */
-        char arr[] = { 'a', 'b', 'c', 'd' };
+        // /* A CHAR ARRAY */
+        // char arr[] = { 'a', 'b', 'c', 'd' };
 
-        /* STRING ARRAY  + WAYS TO INITIALIZE STRINGS */
-        String str = "abcd";
-        String str2 = new String("xyz");
+        // /* STRING ARRAY  + WAYS TO INITIALIZE STRINGS */
+        // String str = "abcd";
+        // String str2 = new String("xyz");
 
-        Scanner sc = new Scanner(System.in);
-        String name;
-        name = sc.nextLine();
-        System.out.println(name);
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine();
+        // System.out.println(name);
+
+        String name = "valhalla";
+        System.out.println(name.length());
     }
 
 }
