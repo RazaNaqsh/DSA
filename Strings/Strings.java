a4794312f94f40068e78a5511ae64b2d0a8ba273
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-01 22:08:42 +0530

    Input and output String

---
+++ Strings/Strings.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Strings {
+    
+    public static void main(String[] args) {
+
+        /* A CHAR ARRAY */
+        char arr[] = { 'a', 'b', 'c', 'd' };
+
+        /* STRING ARRAY  + WAYS TO INITIALIZE STRINGS */
+        String str = "abcd";
+        String str2 = new String("xyz");
+
+        Scanner sc = new Scanner(System.in);
+        String name;
+        name = sc.nextLine();
+        System.out.println(name);
+    }
+
+}
