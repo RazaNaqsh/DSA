641b0e5f1bf3e12c61d7d78ccc7f8c2631807417
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-01 22:17:38 +0530

    Looping thru string

--- Strings/Strings.java
+++ Strings/Strings.java
@@ -1,6 +1,13 @@
 import java.util.*;
 
 public class Strings {
+
+    public static void printLetters(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
+        }
+        System.out.println();
+    }
     
     public static void main(String[] args) {
 
@@ -16,8 +23,10 @@ public class Strings {
         // name = sc.nextLine();
         // System.out.println(name);
 
-        String name = "valhalla";
-        System.out.println(name.length());
+        // String name = "valhalla";
+        // System.out.println(name.length());
+        String gang = "TOKYO MANJI GANG";
+        printLetters(gang);
     }
 
 }
