3bb6599fb0a0d7b0d589256432e27c40d6e0830a
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-04 12:07:15 +0530

    String compression

---
+++ Strings/StringCompression.java
@@ -0,0 +1,20 @@
+public class StringCompression {
+    public static void main(String[] args) {
+        String newStr = "";
+        String str = "aaabbbbccdde";
+        
+        for (int i = 0; i < str.length(); i++) {
+            Integer count = 1;
+
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if (count > 1) {
+                newStr += count.toString();
+            }
+        }
+        System.out.println(newStr);
+    }
+}
