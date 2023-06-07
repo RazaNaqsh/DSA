a7a4450c21b5fe1fd8722dcba78b04238df6fb1d
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-07 20:04:50 +0530

    Oops basics

---
+++ OOPS/OOPSbasic.java
@@ -0,0 +1,30 @@
+public class OOPSbasic {
+    public static void main(String args[]) {
+        Pen p1 = new Pen();
+        p1.setColor("blue");
+        p1.setTip(2);
+        System.out.println(p1.getColor());
+        System.out.println(p1.getTip());
+    }
+}
+
+class Pen {
+    private String color;
+    private int tip;
+
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    void setColor(String newColor){
+        color = newColor;
+    }
+
+    void setTip(int newTip){
+        tip = newTip;
+    }
+}
