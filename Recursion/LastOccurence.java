0bb1c928f246d1218543b47011f310f11fb3ddca
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-12 18:16:29 +0530

    Last occurence

--- Recursion/LastOccurence.java
+++ Recursion/LastOccurence.java
@@ -1,18 +1,20 @@
-public class FirstOccurence {
-    public static int firtOcc(int arr[], int key, int i) {
+public class LastOccurence {
+    public static int lastOcc(int arr[], int key, int i) {
         if (i == arr.length) {
             return -1;
         }
-        if (arr[i] == key) {
+        int isFound = lastOcc(arr, 5, i + 1);
+
+        if (isFound == -1 && arr[i] == key) {
             return i;
         }
 
-        return firtOcc(arr, 5, i + 1);
+        return isFound;
     }
 
     public static void main(String[] args) {
         int arr[] = { 8, 3, 6, 9, 5, 10, 2, 5, 3 };
-        int index = firtOcc(arr, 5, 0);
+        int index = lastOcc(arr, 5, 0);
         if (index != -1) {
             System.out.println("Index is: " + index);
         } else {
