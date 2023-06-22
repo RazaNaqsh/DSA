8f6539be23d64493bf5972ae81fd744d4784dc01
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-22 16:12:39 +0530

    Multi level inheritance

--- OOPS/Inheritance.java
+++ OOPS/Inheritance.java
@@ -1,7 +1,14 @@
 public class Inheritance {
     public static void main(String[] args) {
+        // Single level
         Fish shark = new Fish();
         shark.eat();
+
+        // Multi level inheritance
+        Human joe = new Human();
+        joe.eat();
+        joe.legs = 2;
+        System.out.println(joe.legs);
     }
 }
 
@@ -17,10 +24,22 @@ class Animal {
     }
 }
 
+// Single level inheritance
+
 class Fish extends Animal {
     int fins;
 
     void swim() {
         System.out.println("swims");
     }
+}
+
+// Multi level
+
+class Mammal extends Animal {
+    int legs;
+}
+
+class Human extends Mammal {
+    char gender;
 }
