22c55f58a82051bfa3f2e1b8163f584020c15ae8
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-22 16:34:06 +0530

    Method overriding

---
+++ OOPS/MethodOverriding.java
@@ -0,0 +1,18 @@
+public class MethodOverriding {
+    public static void main(String[] args) {
+        Deer d = new Deer();
+        d.eat();
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("Eats anything");
+    }
+}
+
+class Deer extends Animal {
+    void eat() {
+        System.out.println("Eats grass");
+    }
+}
