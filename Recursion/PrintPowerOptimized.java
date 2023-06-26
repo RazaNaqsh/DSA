1e91919a925ce5da7f146e1b61ceaa30235f8780
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-26 22:04:27 +0530

    Power recursion optimized

--- Recursion/PrintPowerOptimized.java
+++ Recursion/PrintPowerOptimized.java
@@ -7,7 +7,25 @@ public class PrintPowerOptimized {
             return 1;
         }
 
+        // This has time complexity of O(n). because we're calling this function twice.
+
         int halfPowerSq = pow(n, x / 2) * pow(n, x / 2);
+
+        if (x % 2 != 0) {
+            halfPowerSq = n * halfPowerSq;
+        }
+        return halfPowerSq;
+
+    }
+
+    public static int powOptimized(int n, int x) {
+        if (x == 0) {
+            return 1;
+        }
+        // By storing the function call inside a variable. we get the answer, then with
+        // the answer we square it. TC of this code is O(logn).
+        int halfPower = powOptimized(n, x / 2);
+        int halfPowerSq = halfPower * halfPower;
 
         if (x % 2 != 0) {
             halfPowerSq = n * halfPowerSq;
