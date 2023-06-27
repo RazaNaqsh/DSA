d2feb434c72e2d4d93ec0f9e8a093ea100f4892b
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-27 19:33:01 +0530

    Print inc recursion revisited

--- Recursion/PrintIncreasing.java
+++ Recursion/PrintIncreasing.java
@@ -3,12 +3,21 @@ import java.util.*;
 public class PrintIncreasing {
     public static void printDecreasingNumber(int n) {
         if (n == 1) {
-            System.out.println(n);
+            System.out.print(n + " ");
+
             return;
         } else {
             printDecreasingNumber(n - 1);
-            System.out.println(n);
+            System.out.print(n + " ");
         }
+    }
+
+    public static void printInc(int n) {
+        if (n < 1)
+            return;
+        printInc(n - 1);
+        System.out.print(n + " ");
+
     }
 
     public static void main(String args[]) {
@@ -16,5 +25,7 @@ public class PrintIncreasing {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         printDecreasingNumber(n);
+        System.out.println();
+        printInc(n);
     }
 }
