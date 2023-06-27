e320731a2a03054039f8e52f8d03b7da826fc6ec
RazaNaqsh <owaisraza1704@gmail.com>
2023-06-27 19:45:10 +0530

    Factorial recrusion revisited

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -8,10 +8,18 @@ public class Factorial {
         return n * factorial(n - 1);
     }
 
+    static int factRev(int n) {
+        if (n <= 1)
+            return 1;
+        return n * factRev(n - 1);
+    }
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int fact = factorial(n);
         System.out.println("factorial is: " + fact);
+        int rev = factRev(n);
+        System.out.println("factorial Rev is: " + rev);
     }
 }
