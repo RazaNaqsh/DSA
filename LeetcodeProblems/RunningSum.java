83f62830a3445b5974cc92ad5e84ba8610ac2e18
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-05 21:33:49 +0530

    Optimized runningSum code

--- LeetcodeProblems/RunningSum.java
+++ LeetcodeProblems/RunningSum.java
@@ -14,12 +14,25 @@ public class RunningSum {
             }
         }
         return result;
+        // this has a timecomplexity of O(n^2)
+    }
 
+    public static int[] runningSumOptimized(int num[]) {
+        for (int i = 1; i < num.length; i++) {
+            num[i] += num[i - 1];
+        }
+        return num;
+        // this has tc of O(n)
     }
 
     public static void main(String[] args) {
         int num[] = { 1, 2, 3, 4 };
         int result1[] = runningSumRaw(num);
+        System.out.println("unoptimized code: ");
         printArr(result1);
+        System.out.println();
+        System.out.println("optimized code: ");
+        int result2[] = runningSumOptimized(num);
+        printArr(result2);
     }
 }
