ac84908f0781cc0875ae2e74d1a85dbe088db968
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-12 12:42:06 +0530

    GridWays backtracking problem

---
+++ BackTracking/GridWays.java
@@ -0,0 +1,21 @@
+public class GridWays {
+    public static int gridWays(int i, int j, int n, int m) {
+
+        if (i == n - 1 && j == m - 1) {
+            return 1;
+        } else if (i == n || j == m) {
+            return 0;
+        }
+
+        int w1 = gridWays(i + 1, j, n, m); // ways to go right
+        int w2 = gridWays(i, j + 1, n, m); // ways to go down
+
+        return w1 + w2;
+    }
+
+    public static void main(String[] args) {
+        int n = 3, m = 3;
+        System.out.print("Number of ways: ");
+        System.out.println(gridWays(0, 0, n, m));
+    }
+}
