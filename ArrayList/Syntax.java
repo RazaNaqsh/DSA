50d31cb32c5c63a455bc70ef4681cae9ae64dca0
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-13 19:41:29 +0530

    New topic ArrayList and its syntax

---
+++ ArrayList/Syntax.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Syntax {
+    public static void main(String[] args) {
+        ArrayList<Integer> intList = new ArrayList<>();
+        ArrayList<String> strList = new ArrayList<>();
+        ArrayList<Boolean> boolList = new ArrayList<>();
+    }
+}
