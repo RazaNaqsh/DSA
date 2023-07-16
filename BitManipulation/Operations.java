e462c1fb8c738203e6782f841fea8963c65f8d10
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-16 23:17:58 +0530

    Update Ith bit

--- BitManipulation/Operations.java
+++ BitManipulation/Operations.java
@@ -26,11 +26,20 @@ public class Operations {
         System.out.println(num & shiftBit);
     }
 
+    public static void updateBit(int num, int target, int mode) {
+        if (mode == 1) {
+            setBit(num, target);
+        } else {
+            clearBit(num, target);
+        }
+    }
+
     public static void main(String[] args) {
         int num = 6;
         // getBit(num, 2);
         // setBit(num, 1);
-        clearBit(num, 1);
-        clearBit2(num, 1);
+        // clearBit(num, 1);
+        // clearBit2(num, 1);
+        updateBit(num, 0, 1);
     }
 }
