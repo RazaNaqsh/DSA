7bdff0d585169ab872d723fcfc1d5fed6d4fa57a
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-20 23:12:23 +0530

    String compression using StringBuilder

--- Strings/StringCompression.java
+++ Strings/StringCompression.java
@@ -1,20 +1,40 @@
 public class StringCompression {
-    public static void main(String[] args) {
-        String newStr = "";
-        String str = "aaabbbbccdde";
-        
+    public static String compress(String str) {
+        StringBuilder newStr = new StringBuilder("");
+
         for (int i = 0; i < str.length(); i++) {
             Integer count = 1;
-
             while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
                 count++;
                 i++;
             }
-            newStr += str.charAt(i);
+            newStr.append(str.charAt(i));
             if (count > 1) {
-                newStr += count.toString();
+                newStr.append(count);
             }
         }
-        System.out.println(newStr);
+        return newStr.toString();
+    }
+
+    public static void main(String[] args) {
+        // String newStr = "";
+        String str = "aaabbbbccdde";
+
+        String result = compress(str);
+        System.out.println(result);
+
+        // for (int i = 0; i < str.length(); i++) {
+        // Integer count = 1;
+
+        // while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+        // count++;
+        // i++;
+        // }
+        // newStr += str.charAt(i);
+        // if (count > 1) {
+        // newStr += count.toString();
+        // }
+        // }
+        // System.out.println(newStr);
     }
 }
