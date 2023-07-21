54409266a8fa92f9e9c5076557d8f3cf93c1fa42
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-21 16:13:45 +0530

    add in the middle in LL

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -51,15 +51,35 @@ public class LinkedList {
         System.out.println("null");
     }
 
+    public void add(int idx, int data) {
+
+        if (idx == 0) {
+            addFirst(data);
+            return;
+        }
+
+        Node newNode = new Node(data);
+        Node temp = head;
+        int i = 0;
+
+        while (i < idx - 1) {
+            temp = temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.print();
         ll.addFirst(2);
         ll.addFirst(1);
-        ll.print();
 
         ll.addLast(3);
         ll.addLast(4);
+
+        ll.add(2, 9);
         ll.print();
 
     }
