90a5e3986426218b3519165366dc72225cb7051a
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-21 11:03:14 +0530

    Add first in linked list

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -14,7 +14,28 @@ public class LinkedList {
     public static Node head;
     public static Node tail;
 
+    public void addFirst(int data) {
+        // Create new node
+        Node newNode = new Node(data);
+
+        // if linked list was empty then initialize it with the new node
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+        // point new node to next node which was currenthead
+        newNode.next = head;
+
+        // Now change the head to the currentnode
+        head = newNode;
+    }
+
+    // methods
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
+
+        ll.addFirst(1);
+        ll.addFirst(2);
+
     }
 }
