662d03ad324fcd2f0dc0cafb0506aeb1de13f5e8
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-21 10:18:52 +0530

    Creating LinkedList class

---
+++ LinkedList/LinkedList.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+public class LinkedList {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
