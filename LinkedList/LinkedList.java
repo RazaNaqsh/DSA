580467e871f840649c8f83451f7d687ef613f282
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-23 16:40:56 +0530

    Check palindrome using slowfast approach

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -184,33 +184,61 @@ public class LinkedList {
             prev = prev.next;
             i++;
         }
-
         prev.next = prev.next.next;
-
         return;
+    }
 
+    public Node findMid(Node head) {
+        // slow fast approach
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    public boolean checkPalindrom() {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        // step1 - find mid
+        Node midNode = findMid(head);
+
+        // step2 - reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev; // right half head
+        Node left = head;
+
+        // step3 - check left half and right half
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         LinkedList ll = new LinkedList();
-        ll.addFirst(2);
-        ll.addFirst(1);
-
-        ll.addLast(4);
-        ll.addLast(5);
-
-        ll.add(2, 3);
-        ll.print();
-        System.out.println(ll.size);
-
-        ll.removeLast();
-        ll.print();
-
-        System.out.println(ll.recSearch(2));
-
-        ll.reverse();
+        ll.addLast(1);
+        ll.addLast(2);
+        ll.addLast(2);
+        ll.addLast(1);
 
         ll.print();
+        System.out.println(ll.checkPalindrom());
 
     }
 }
