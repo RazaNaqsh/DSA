2b28a1c60640459bc9a5a558255d9b47019e78ee
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-26 18:29:17 +0530

    Remove first method

--- LinkedList/DoubleLL.java
+++ LinkedList/DoubleLL.java
@@ -30,6 +30,25 @@ public class DoubleLL {
         head = newNode;
     }
 
+    public int removeFirst() {
+        if (head == null) {
+            System.out.println("empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            return val;
+        }
+
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
     public void print() {
         Node temp = head;
         while (temp != null) {
