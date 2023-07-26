a237b0a6b93643ea198b13920df38cca7f7b3488
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-26 18:20:42 +0530

    Fix constructor

--- LinkedList/DoubleLL.java
+++ LinkedList/DoubleLL.java
@@ -18,18 +18,35 @@ public class DoubleLL {
     public static int size;
 
     public void addFirst(int data) {
-        Node newNode = new Node(1);
+        Node newNode = new Node(data);
+        size++;
 
         if (head == null) {
             head = tail = newNode;
+            return;
         }
         newNode.next = head;
         head.prev = newNode;
         head = newNode;
     }
 
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
     public static void main(String[] args) {
         DoubleLL dll = new DoubleLL();
+        dll.addFirst(3);
+        dll.addFirst(2);
+        dll.addFirst(1);
+
+        dll.print();
+        System.out.println(dll.size);
 
     }
 }
