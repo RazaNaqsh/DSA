084a61d035ae92495b73391315230c84219441eb
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-27 18:00:40 +0530

    Static way to access stack

--- StackDs/StackLL.java
+++ StackDs/StackLL.java
@@ -50,13 +50,21 @@ public class StackLL {
 
     public static void main(String[] args) {
         Stack s = new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
 
-        while (!s.isEmpty()) {
-            System.out.println(s.peek());
-            s.pop();
+        // while (!s.isEmpty()) {
+        // System.out.println(s.peek());
+        // s.pop();
+        // }
+
+        Stack.push(1);
+        Stack.push(2);
+
+        while (!Stack.isEmpty()) {
+            System.out.println(Stack.peek());
+            Stack.pop();
         }
     }
 }
