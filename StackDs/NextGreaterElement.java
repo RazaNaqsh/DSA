a7d3344bff3463049cecbd33d04929bbbdeb36ff
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-30 12:03:24 +0530

    Next greater element

---
+++ StackDs/NextGreaterElement.java
@@ -0,0 +1,27 @@
+package StackDs;
+
+import java.util.*;
+
+public class NextGreaterElement {
+
+    public static void main(String[] args) {
+        int arr[] = { 6, 8, 0, 1, 3 };
+        int nextGreater[] = new int[arr.length];
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] <= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nextGreater[i] = -1;
+            } else {
+                nextGreater[i] = arr[s.peek()];
+            }
+            s.push(i);
+        }
+
+        for (int i = 0; i < nextGreater.length; i++) {
+            System.out.print(nextGreater[i] + " ");
+        }
+    }
+}
