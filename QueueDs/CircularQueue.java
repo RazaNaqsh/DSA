fe9f15444ad775a1edd49bf2a5841dd6b2b66ed5
RazaNaqsh <owaisraza1704@gmail.com>
2023-07-31 20:11:15 +0530

    Circular queue using array

---
+++ QueueDs/CircularQueue.java
@@ -0,0 +1,69 @@
+package QueueDs;
+
+public class CircularQueue {
+    static class Queue {
+        static int arr[];
+        static int rear;
+        static int front;
+        static int size;
+
+        Queue(int n) {
+            arr = new int[n];
+            size = n;
+            rear = -1;
+            front = -1;
+        }
+
+        public static boolean isEmpty() {
+            return front == -1 && rear == -1;
+        }
+
+        public static boolean isFull() {
+            return (rear + 1) % size == front;
+        }
+
+        public static void add(int data) {
+            if (isFull()) {
+                System.out.println("Queue is already FUll");
+                return;
+            }
+            if (front == -1)
+                front = 0;
+            rear = (rear + 1) % size;
+            arr[rear] = data;
+        }
+
+        public static int remove() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            int result = arr[front];
+            if (rear == front) {
+                front = rear = -1;
+            } else {
+                front = (front + 1) % size;
+            }
+            return result;
+        }
+
+        public static int peek() {
+            if (isEmpty()) {
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    public static void main(String[] args) {
+        Queue q = new Queue(5);
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.remove());
+        }
+    }
+}
