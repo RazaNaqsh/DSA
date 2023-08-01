194a88137dec1d008d45efdb20ddd5fa78814fb8
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-01 20:10:14 +0530

    Push method for stack using 2 queues

--- QueueDs/StackUsing2Queues.java
+++ QueueDs/StackUsing2Queues.java
@@ -11,6 +11,62 @@ import java.util.*;
 
 public class StackUsing2Queues {
     // This is Pop[O(n)] method
+    // static class Stack {
+    // static Queue<Integer> q1 = new LinkedList<>();
+    // static Queue<Integer> q2 = new LinkedList<>();
+
+    // public static boolean isEmpty() {
+    // return q1.isEmpty() && q2.isEmpty();
+    // }
+
+    // public static void push(int data) {
+    // if (!q1.isEmpty()) {
+    // q1.add(data);
+    // } else {
+    // q2.add(data);
+    // }
+    // }
+
+    // public static int pop() {
+    // int top = -1;
+    // if (!q1.isEmpty()) {
+    // while (!q1.isEmpty()) {
+    // top = q1.remove();
+    // if (q1.isEmpty()) {
+    // break;
+    // }
+    // q2.add(top);
+    // }
+    // } else {
+    // while (!q2.isEmpty()) {
+    // top = q2.remove();
+    // if (q2.isEmpty()) {
+    // break;
+    // }
+    // q1.add(top);
+    // }
+    // }
+    // return top;
+    // }
+
+    // public static int peek() {
+    // int top = -1;
+    // if (!q1.isEmpty()) {
+    // while (!q1.isEmpty()) {
+    // top = q1.remove();
+    // q2.add(top);
+    // }
+    // } else {
+    // while (!q2.isEmpty()) {
+    // top = q2.remove();
+    // q1.add(top);
+    // }
+    // }
+    // return top;
+    // }
+    // }
+
+    // * This is push[O(n)] method where push takes linear tc rest constant
     static class Stack {
         static Queue<Integer> q1 = new LinkedList<>();
         static Queue<Integer> q2 = new LinkedList<>();
@@ -20,49 +76,29 @@ public class StackUsing2Queues {
         }
 
         public static void push(int data) {
-            if (!q1.isEmpty()) {
-                q1.add(data);
-            } else {
-                q2.add(data);
+            while (!q1.isEmpty()) {
+                q2.add(q1.remove());
+            }
+            q1.add(data);
+            while (!q2.isEmpty()) {
+                q1.add(q2.remove());
             }
         }
 
         public static int pop() {
-            int top = -1;
-            if (!q1.isEmpty()) {
-                while (!q1.isEmpty()) {
-                    top = q1.remove();
-                    if (q1.isEmpty()) {
-                        break;
-                    }
-                    q2.add(top);
-                }
-            } else {
-                while (!q2.isEmpty()) {
-                    top = q2.remove();
-                    if (q2.isEmpty()) {
-                        break;
-                    }
-                    q1.add(top);
-                }
+            if (q1.isEmpty()) {
+                System.out.println("Stack is Empty");
+                return -1;
             }
-            return top;
+            return q1.remove();
         }
 
         public static int peek() {
-            int top = -1;
-            if (!q1.isEmpty()) {
-                while (!q1.isEmpty()) {
-                    top = q1.remove();
-                    q2.add(top);
-                }
-            } else {
-                while (!q2.isEmpty()) {
-                    top = q2.remove();
-                    q1.add(top);
-                }
+            if (q1.isEmpty()) {
+                System.out.println("Stack is Empty");
+                return -1;
             }
-            return top;
+            return q1.peek();
         }
     }
 
