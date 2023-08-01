8a197afb9971af49fb235b4de33539b39fe48029
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-01 18:28:57 +0530

    Pop O(n) way of Queue using 2 stacks

--- QueueDs/QueueUsing2Stacks.java
+++ QueueDs/QueueUsing2Stacks.java
@@ -5,8 +5,44 @@ import java.util.*;
 make a queue of 2 stacks where add operation takes O(n) and rest operations O(1).
 Other way is where remove and peek takes O(n) and add takes O(1)  */
 
-// * This is where add takes O(n) TC and rest O(1).
 public class QueueUsing2Stacks {
+    // * This is where add takes O(n) TC and rest O(1).
+    // static class Queue {
+    // static Stack<Integer> s1 = new Stack<>();
+    // static Stack<Integer> s2 = new Stack<>();
+
+    // public static boolean isEmpty() {
+    // return s1.isEmpty();
+    // }
+
+    // public static void add(int data) {
+    // while (!s1.isEmpty()) {
+    // s2.push(s1.pop());
+    // }
+    // s1.push(data);
+    // while (!s2.isEmpty()) {
+    // s1.push(s2.pop());
+    // }
+    // }
+
+    // public static int remove() {
+    // if (s1.isEmpty()) {
+    // System.out.println("Queue is Empty");
+    // return -1;
+    // }
+    // return s1.pop();
+    // }
+
+    // public static int peek() {
+    // if (s1.isEmpty()) {
+    // System.out.println("Queue is Empty");
+    // return -1;
+    // }
+    // return s1.peek();
+    // }
+    // }
+
+    // * This one is where remove and peek takes O(n) and add O(1).
     static class Queue {
         static Stack<Integer> s1 = new Stack<>();
         static Stack<Integer> s2 = new Stack<>();
@@ -16,29 +52,31 @@ public class QueueUsing2Stacks {
         }
 
         public static void add(int data) {
-            while (!s1.isEmpty()) {
-                s2.push(s1.pop());
-            }
             s1.push(data);
-            while (!s2.isEmpty()) {
-                s1.push(s2.pop());
-            }
         }
 
         public static int remove() {
-            if (s1.isEmpty()) {
-                System.out.println("Queue is Empty");
-                return -1;
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
             }
-            return s1.pop();
+            int result = s2.pop();
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+
+            return result;
         }
 
         public static int peek() {
-            if (s1.isEmpty()) {
-                System.out.println("Queue is Empty");
-                return -1;
+            while (!s1.isEmpty()) {
+                s2.push(s1.pop());
             }
-            return s1.peek();
+            int result = s2.peek();
+            while (!s2.isEmpty()) {
+                s1.push(s2.pop());
+            }
+
+            return result;
         }
     }
 
---
+++ QueueDs/StackUsing2Queues.java
@@ -0,0 +1,7 @@
+package QueueDs;
+
+public class StackUsing2Queues {
+    public static void main(String[] args) {
+
+    }
+}
