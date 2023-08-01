93847ac86636ba1aeb17e17c66e06e39425c698c
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-01 17:56:27 +0530

    also using arraydeque way

--- QueueDs/QueueJFC.java
+++ QueueDs/QueueJFC.java
@@ -4,7 +4,10 @@ import java.util.*;
 
 public class QueueJFC {
     public static void main(String[] args) {
-        Queue<Integer> q = new LinkedList<>();
+        // Since queue in itself is not a class, its kind of an interface
+        // which is implemented using these below 2 classes.
+        // Queue<Integer> q = new LinkedList<>();
+        Queue<Integer> q = new ArrayDeque<>();
 
         q.add(1);
         q.add(2);
