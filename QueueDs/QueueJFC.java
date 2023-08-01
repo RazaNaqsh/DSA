6f2ec64c11e88b3dda90d938d134473f7f8d20b0
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-01 17:54:08 +0530

    Using Queue from jfc

---
+++ QueueDs/QueueJFC.java
@@ -0,0 +1,18 @@
+package QueueDs;
+
+import java.util.*;
+
+public class QueueJFC {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(1);
+        q.add(2);
+        q.add(3);
+
+        while (!q.isEmpty()) {
+            System.out.println(q.remove());
+        }
+
+    }
+}
