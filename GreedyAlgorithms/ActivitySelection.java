8758997478589475d901f3ab50f68f4c78105edb
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-04 15:36:16 +0530

    Activity selection but sorting the arrays first

--- GreedyAlgorithms/ActivitySelection.java
+++ GreedyAlgorithms/ActivitySelection.java
@@ -2,21 +2,33 @@ import java.util.*;
 
 public class ActivitySelection {
     public static void actSelection(int s[], int e[]) {
+
+        // sorting
+        int activities[][] = new int[s.length][3];
+        for (int i = 0; i < s.length; i++) {
+            activities[i][0] = i;
+            activities[i][1] = s[i];
+            activities[i][2] = e[i];
+        }
+        // Lambda function -> shortform
+        // * Comparator is an interface for comparing objects in java.
+        Arrays.sort(activities, Comparator.comparingDouble(o -> o[2]));
+
         // end time basis sorted
         int maxAct = 0;
         ArrayList<Integer> ans = new ArrayList<>();
 
         // 1st activity
         maxAct = 1;
-        ans.add(0);
-        int lastEnd = e[0];
+        ans.add(activities[0][0]);
+        int lastEnd = activities[0][2];
 
         // Counting activity
         for (int i = 1; i < e.length; i++) {
-            if (s[i] >= lastEnd) {
+            if (activities[i][1] >= lastEnd) {
                 maxAct++;
-                ans.add(i);
-                lastEnd = e[i];
+                ans.add(activities[i][0]);
+                lastEnd = activities[i][2];
             }
         }
 
