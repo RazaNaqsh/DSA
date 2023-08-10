b6bed18b94c20b940b2100c136fdcdf56e81019c
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-10 13:56:54 +0530

    Subtree of another tree

--- BinaryTrees/BinaryTreeCode.java
+++ BinaryTrees/BinaryTreeCode.java
@@ -157,16 +157,60 @@ public class BinaryTreeCode {
         }
     }
 
+    public static boolean isIdentical(Node node, Node subRoot) {
+        if (node == null && subRoot == null) {
+            return true;
+        } else if (node == null || subRoot == null || node.data != subRoot.data) {
+            return false;
+        }
+
+        if (!isIdentical(node.left, subRoot.left)) {
+            return false;
+        }
+        if (!isIdentical(node.right, subRoot.right)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isSubtree(Node root, Node subRoot) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == subRoot.data) {
+            if (isIdentical(root, subRoot)) {
+                return true;
+            }
+        }
+        return isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
     public static void main(String[] args) {
-        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
-        BinaryTree tree = new BinaryTree();
-        Node root = tree.buildTree(nodes);
+        // int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, 7, -1, -1, 6, -1, -1 };
+        // BinaryTree tree = new BinaryTree();
+        // Node root = tree.buildTree(nodes);
         // System.out.println(root.data);
 
         // tree.preorder(root);
         // tree.inorder(root);
         // tree.postorder(root);
         // tree.levelOrder(root);
-        System.out.println((tree.diam2(root)).diam);
+        // subTree.levelOrder(subRoot);
+        // System.out.println((tree.diam2(root)).diam);
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        Node subRoot = new Node(2);
+        subRoot.left = new Node(4);
+        subRoot.right = new Node(5);
+
+        System.out.println(isSubtree(root, subRoot));
+
     }
 }
