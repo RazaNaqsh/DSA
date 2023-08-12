5e6eaa350f3bca81f0936428f1b53107df3ad96c
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-12 10:46:01 +0530

    Kth level

---
+++ BinaryTrees/KthLevel.java
@@ -0,0 +1,49 @@
+package BinaryTrees;
+
+import java.util.*;
+
+public class KthLevel {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void klevel(Node root, int lvl, int k) {
+        if (root == null) {
+            return;
+        }
+        if (lvl == k) {
+            System.out.print(root.data + " ");
+            return;
+        }
+        klevel(root.left, lvl + 1, k);
+        klevel(root.right, lvl + 1, k);
+    }
+
+    public static void main(String[] args) {
+        /*
+         * tree
+         * 1
+         * / \
+         * 2 3
+         * / \ / \
+         * 4 5 6 7
+         */
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        klevel(root, 1, 3);
+    }
+}
