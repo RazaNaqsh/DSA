a1f25faf2b2d6285f63be8d6989ac28ae342aa95
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-13 16:04:35 +0530

    Delete a node and its three cases

--- BST/BST.java
+++ BST/BST.java
@@ -46,8 +46,40 @@ public class BST {
             return search(root.right, key);
     }
 
+    public static Node delete(Node root, int val) {
+        if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else {
+            // case 1 - leaf Node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+            // case 2 - single child
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            // case 3 - both children
+            Node IS = findIS(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node findIS(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        int vals[] = { 5, 1, 3, 4, 2, 7 };
+        int vals[] = { 8, 5, 3, 1, 4, 6, 10, 11, 14 };
         Node root = null;
         for (int i = 0; i < vals.length; i++) {
             root = insert(root, vals[i]);
@@ -57,10 +89,13 @@ public class BST {
 
         System.out.println();
 
-        if (search(root, 6)) {
-            System.out.println("Found");
-        } else {
-            System.out.println("Not found");
-        }
+        // if (search(root, 6)) {
+        // System.out.println("Found");
+        // } else {
+        // System.out.println("Not found");
+        // }
+
+        delete(root, 10);
+        inorder(root);
     }
 }
