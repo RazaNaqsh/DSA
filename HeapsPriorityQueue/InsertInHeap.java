c72b896b7978b4ac4e9480319fb78b0013b1e994
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-21 20:00:36 +0530

    Remove from Heap

--- HeapsPriorityQueue/InsertInHeap.java
+++ HeapsPriorityQueue/InsertInHeap.java
@@ -15,15 +15,82 @@ public class InsertInHeap {
                 int temp = arr.get(x);
                 arr.set(x, arr.get(par));
                 arr.set(par, temp);
+
+                x = par;
+                par = (x - 1) / 2;
             }
         }
 
         public int peek() {
             return arr.get(0);
         }
+
+        /*
+         * How to delete in Heap:
+         * its a 3 step process where first we:
+         * -Swap 1st and last node
+         * -Remove last Node
+         * -Fix the heap using another function heapify
+         * 
+         * How does Heapify work?
+         * -use the special property, root=i, left = 2i+1 , right = 2i+2
+         * -then find min out of three
+         * -swap root with min
+         * -and its a recursive function
+         */
+        public void heapify(int i) {
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            int minIdx = i;
+
+            if (left < arr.size() && arr.get(minIdx) > arr.get(left)) {
+                minIdx = left;
+            }
+            if (right < arr.size() && arr.get(minIdx) > arr.get(right)) {
+                minIdx = right;
+            }
+
+            if (minIdx != i) {
+                int temp = arr.get(i);
+                arr.set(i, arr.get(minIdx));
+                arr.set(minIdx, temp);
+
+                heapify(minIdx);
+            }
+
+        }
+
+        public int remove() {
+            int data = arr.get(0);
+
+            // step1 - Swap first with last
+            int temp = arr.get(0);
+            arr.set(0, arr.get(arr.size() - 1));
+            arr.set(arr.size() - 1, temp);
+
+            // step2 - remove last node
+            arr.remove(arr.size() - 1);
+
+            // Heapify
+            heapify(0);
+            return data;
+        }
+
+        public boolean isEmpty() {
+            return arr.size() == 0;
+        }
     }
 
     public static void main(String[] args) {
+        Heap h = new Heap();
+        h.add(3);
+        h.add(4);
+        h.add(1);
+        h.add(5);
 
+        while (!h.isEmpty()) {
+            System.out.println(h.peek());
+            h.remove();
+        }
     }
 }
