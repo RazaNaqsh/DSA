112869c0cc77966775032d3d2cac0d3643ca2ebc
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-27 16:08:45 +0530

    Adding data in hashmaps

---
+++ Hashmaps/Hashmaps.java
@@ -0,0 +1,15 @@
+package Hashmaps;
+
+import java.util.*;
+
+public class Hashmaps {
+    public static void main(String[] args) {
+        HashMap<String, Integer> hm = new HashMap<>();
+
+        hm.put("India", 100);
+        hm.put("US", 50);
+        hm.put("Japan", 30);
+
+        System.out.println(hm);
+    }
+}
