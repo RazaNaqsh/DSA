202182c633403423babdd5bc172df0e7366fbe84
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-27 16:20:35 +0530

    Get operation

--- Hashmaps/Hashmap.java
+++ Hashmaps/Hashmap.java
@@ -2,14 +2,21 @@ package Hashmaps;
 
 import java.util.*;
 
-public class Hashmaps {
+public class Hashmap {
     public static void main(String[] args) {
         HashMap<String, Integer> hm = new HashMap<>();
 
+        // Insert - o(1)
         hm.put("India", 100);
         hm.put("US", 50);
         hm.put("Japan", 30);
 
         System.out.println(hm);
+
+        // Get - o(1)
+        int population = hm.get("India");
+        System.out.println(population);
+
+        System.out.println(hm.get("China"));
     }
 }
