0b072c2af6e82964ff5c1b490dd4cc801513d445
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-29 17:59:35 +0530

    Put implementation of HashMap

--- Hashmaps/HashMapCode.java
+++ Hashmaps/HashMapCode.java
@@ -14,20 +14,71 @@ public class HashMapCode {
             }
         }
 
-        private int size;
+        private int n;
+        private int N;
         private LinkedList<Node> buckets[];
 
         @SuppressWarnings("unchecked")
         public HashMap() {
-            this.size = 0;
-            this.buckets = new LinkedList[4];
+            this.N = 4;
+            this.buckets = new LinkedList[4]; // in some versions of java we cannot create a ll without
+            // giving a type, thus java doesnt let us run the program without defining , so
+            // we supress the warning
             for (int i = 0; i < 4; i++) {
                 this.buckets[i] = new LinkedList<>();
             }
         }
 
-        public void put(K key, V value) {
+        private int hashFunction(K key) {
+            int hc = key.hashCode();
+            return Math.abs(hc) % N;
+        }
 
+        private int SearchInLL(K key, int bi) {
+            LinkedList<Node> ll = buckets[bi];
+            int di = 0;
+
+            for (int i = 0; i < ll.size(); i++) {
+                Node node = ll.get(i);
+                if (node.key == key) {
+                    return di;
+                }
+                di++;
+            }
+            return -1;
+
+        }
+
+        private void rehash() {
+            LinkedList<Node> oldBuck[] = buckets;
+            buckets = new LinkedList[N * 2];
+            N = 2 * N;
+            for (int i = 0; i < buckets.length; i++) {
+                buckets[i] = new LinkedList<>();
+            }
+
+            // nodes > add in bucket
+            for (int i = 0; i < oldBuck.length; i++) {
+                LinkedList<Node> ll = oldBuck[i];
+                for (int j = 0; j < ll.size(); j++) {
+                    Node node = ll.remove();
+                    put(node.key, node.value);
+                }
+            }
+        }
+
+        public void put(K key, V value) {
+            int bi = hashFunction(key);
+            int di = SearchInLL(key, bi);
+
+            if (di != -1) {
+                Node node = buckets[bi].get(di);
+                node.value = value;
+            } else {
+                buckets[bi].add(new Node(key, value));
+                n++;
+            }
+            double lambda = (double) n / N;
         }
 
         public boolean containsKey(K key) {
