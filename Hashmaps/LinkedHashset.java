4d3d94f61ea2c42f8af2c8f84a963452f27acb5d
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-30 17:36:20 +0530

    Linked HashSet

---
+++ Hashmaps/LinkedHashset.java
@@ -0,0 +1,14 @@
+package Hashmaps;
+
+import java.util.*;
+
+public class LinkedHashset {
+    public static void main(String[] args) {
+        LinkedHashSet<String> lhs = new LinkedHashSet<>();
+        lhs.add("Delhi");
+        lhs.add("Ranchi");
+        lhs.add("Mumbai");
+
+        System.out.println(lhs);
+    }
+}
