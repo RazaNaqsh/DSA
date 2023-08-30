cbcc62428f2ecea0b87c970812924fb53f3ceffb
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-30 17:50:18 +0530

    Count Distinct Element

---
+++ Hashmaps/CountDistinctElement.java
@@ -0,0 +1,16 @@
+package Hashmaps;
+
+import java.util.*;
+
+public class CountDistinctElement {
+    public static void main(String[] args) {
+        int num[] = { 4, 3, 2, 5, 6, 7, 3, 4, 2, 1 };
+        HashSet<Integer> set = new HashSet<>();
+
+        for (int i = 0; i < num.length; i++) {
+            set.add(num[i]);
+        }
+
+        System.out.println("Count of Distinct Element: " + set.size());
+    }
+}
