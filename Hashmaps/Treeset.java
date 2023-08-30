1004ad5717b3d82b8a74da6a3d94ce12725c4e31
RazaNaqsh <owaisraza1704@gmail.com>
2023-08-30 17:41:41 +0530

    TreeSet

---
+++ Hashmaps/Treeset.java
@@ -0,0 +1,17 @@
+package Hashmaps;
+
+import java.util.*;
+
+public class Treeset {
+    public static void main(String[] args) {
+        TreeSet<String> ts = new TreeSet<>();
+
+        ts.add("Delhi");
+        ts.add("NewYork");
+        ts.add("Paris");
+        ts.add("Ranchi");
+        ts.add("London");
+
+        System.out.println(ts);
+    }
+}
