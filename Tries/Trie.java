436afd88aa0c5d4b64546a4630794dcda757ccb5
RazaNaqsh <owaisraza1704@gmail.com>
2023-09-01 15:20:40 +0530

    Word Break problem

--- Tries/Trie.java
+++ Tries/Trie.java
@@ -38,13 +38,27 @@ public class Trie {
         return curr.eow == true;
     }
 
+    public static boolean wordBreak(String key) {
+        if (key.length() == 0) {
+            return true;
+        }
+        for (int i = 1; i <= key.length(); i++) {
+            if (search(key.substring(0, i)) &&
+                    wordBreak(key.substring(i))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public static void main(String[] args) {
-        String words[] = { "the", "a", "there", "their", "any", "thee" };
+        String words[] = { "i", "like", "sam", "samsung", "mobile", "ice" };
 
         for (int i = 0; i < words.length; i++) {
             insert(words[i]);
         }
-        System.out.println(search("thee"));
+        String key = "ilikesamsung";
 
+        System.out.println(wordBreak(key));
     }
 }
