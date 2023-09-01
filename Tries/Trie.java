76f8b19e71fbf641e3adce9fe84f2d20b20a2df8
RazaNaqsh <owaisraza1704@gmail.com>
2023-09-01 11:48:52 +0530

    Inserting in trie

---
+++ Tries/Trie.java
@@ -0,0 +1,36 @@
+package Tries;
+
+public class Trie {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node() {
+            for (int i = 0; i < 26; i++) {
+                children[i] = null;
+            }
+        }
+    }
+
+    public static Node root = new Node();
+
+    public static void insert(String word) { // o(l) l = largest word
+        Node curr = root;
+        for (int level = 0; level < word.length(); level++) {
+            int idx = word.charAt(level) - 'a';
+            if (curr.children[idx] == null) {
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+
+    public static void main(String[] args) {
+        String words[] = { "the", "a", "there", "their", "any", "thee" };
+
+        for (int i = 0; i < words.length; i++) {
+            insert(words[i]);
+        }
+    }
+}
