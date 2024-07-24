c7ac3629e5bd6276e6ef5b5921690192b50a8164
RazaNaqsh <owaisraza1704@gmail.com>
2024-07-24 18:29:37 +0530

    Number matrix

---
+++ StriverDsa/patterns/numberMatrix.py
@@ -0,0 +1,13 @@
+def getNumberPattern(n: int) -> None:
+    size = 2*n-1
+    for i in range(size):
+        for j in range(size):
+            top = i
+            left = j
+            right = size - j- 1
+            bottom = size - i - 1
+            min_dist = min(top,bottom,right,left)
+            print(n-min_dist,end="")
+        print()
+
+getNumberPattern(4)
